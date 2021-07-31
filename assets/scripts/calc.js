6c3245fdb45180cbf7d739a58e71ba2d05e4bb40
DrewDaniels <drew@thedaniels.com>
2021-07-30 19:08:13 -0500

    added keyboard support

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -143,32 +143,16 @@ btns.forEach(b => b.addEventListener('click', function() {
 
 }))
 // add functionality to detect keystrokes also
-
-
-//create a function to append all button clicks to a common string
-//where we can use regex to parse out the numbers separated by an operator
-//followed by another number
-const lineBreak = () => console.log('-'.repeat(40))
-const funcDesc = (func) => console.log(`-------------${func}-------------`)
-
-function selfTest() {
-    funcDesc('ADD')
-    console.log(add(2, 4));
-    console.log(add(50, 50));
-    funcDesc('SUBTRACT')
-    console.log(subtract(2, 4));
-    console.log(subtract(100, 50));
-    funcDesc('MULTIPLY');
-    console.log(multiply(2, 4));
-    console.log(multiply(100, 50));
-    funcDesc('DIVIDE');
-    console.log(divide(4, 2));
-    console.log(divide(100, 50));
-    funcDesc('OPERATOR');
-    console.log(operate('2','+','2'))
-    console.log(operate('2','-','2'))
-    console.log(operate('2','*','2'))
-    console.log(operate('2','/','2'))
-}
+document.addEventListener('keydown', (e) => {
+    let btn;
+    let key = e.key;
+    if (numsAndOps.includes(key) || key === '=') {
+        btn = document.getElementById(key);
+        btn.click();
+    } else if (key == '='||key == 'Enter') {
+        btn = document.getElementById('equals');
+        btn.click();
+    } else console.log(key);
+})
 
 
